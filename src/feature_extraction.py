ae86a347c435b601d56ac4c6afda631a6e21a263
Marco Trentini <marcotr@ethz.ch>
2024-07-22 12:44:07 +0200

    working detection with elevation map

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -3,7 +3,6 @@
 import rospy
 from visualization_msgs.msg import MarkerArray, Marker
 from grid_map_msgs.msg import GridMap
-from std_msgs.msg import Float32MultiArray
 import numpy as np
 from sklearn.cluster import DBSCAN
 
@@ -18,7 +17,7 @@ position_y_global = 0
 
 def callback(grid_map):
     global latest_centroids, frame_id_global, length_x_global, length_y_global, resolution_global, position_x_global, position_y_global
-    elevation_layer = grid_map.layers.index('elevation')
+    elevation_layer = grid_map.layers.index('upper_bound')
     frame_id_global = grid_map.info.header.frame_id
     resolution_global = float(grid_map.info.resolution)
     length_x_global = float(grid_map.info.length_x)
@@ -31,23 +30,34 @@ def callback(grid_map):
     
     # Extracting raw points before clustering
     raw_points = extract_raw_points(elevation_data)
-    publish_raw_points(frame_id_global, resolution_global, length_x_global, length_y_global, raw_points)
-    
     latest_centroids = define_rock_centroids(elevation_data)
 
+    publish_raw_points(frame_id_global, resolution_global, length_x_global, length_y_global, raw_points)
+    publish_markers(frame_id_global, resolution_global, length_x_global, length_y_global, latest_centroids)
+
 def extract_raw_points(elevation_data):
-    lower_elevation_threshold = -0.3
-    upper_elevation_threshold = 0.2
+    lower_elevation_threshold = -0.25
+    upper_elevation_threshold = 0
     rock_mask = (elevation_data > lower_elevation_threshold) & (elevation_data < upper_elevation_threshold)
     
     x, y = np.where(rock_mask)
     points = np.column_stack((x, y))
     return points
 
+def calculate_distance(point1, point2):
+    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+
+def is_within_radius(new_centroid, centroids, radius):
+    for centroid in centroids:
+        if calculate_distance(new_centroid, centroid) < radius:
+            return True
+    return False
+
 def define_rock_centroids(elevation_data):
     points = extract_raw_points(elevation_data)
     
-    clustering = DBSCAN(eps=4, min_samples=4).fit(points)
+    clustering = DBSCAN(eps=1, min_samples=4).fit(points)
     labels = clustering.labels_
     
     unique_labels = set(labels)
@@ -56,9 +66,10 @@ def define_rock_centroids(elevation_data):
         if label != -1:
             class_member_mask = (labels == label)
             class_points = points[class_member_mask]
-            if 3 <= len(class_points) <= 25:
-                centroid = np.mean(class_points, axis=0)
-                centroids.append((int(centroid[0]), int(centroid[1]), 0))
+            if 15 <= len(class_points) <= 300:
+                new_centroid = np.mean(class_points, axis=0)
+                if not is_within_radius(new_centroid, centroids, 35):
+                    centroids.append((int(new_centroid[0]), int(new_centroid[1]), 0))
     
     return centroids
 
@@ -81,7 +92,7 @@ def publish_raw_points(frame_id, resolution, length_x, length_y, points):
         marker.color.b = 0.0
         marker.color.a = 1.0
         marker.pose.orientation.w = 1.0
-        marker.lifetime = rospy.Duration(0.5)
+        marker.lifetime = rospy.Duration()
         
         world_x = -point[1] * resolution + offset_y + position_x_global
         world_y = -point[0] * resolution + offset_x + position_y_global
@@ -114,7 +125,7 @@ def publish_markers(frame_id, resolution, length_x, length_y, centroids):
             marker.color.b = 0.0
             marker.color.a = 1.0
             marker.pose.orientation.w = 1.0
-            marker.lifetime = rospy.Duration(0.5)
+            marker.lifetime = rospy.Duration()
             all_markers.markers.append(marker)
     elif centroid_count < current_marker_count:
         del all_markers.markers[centroid_count:]
@@ -130,17 +141,12 @@ def publish_markers(frame_id, resolution, length_x, length_y, centroids):
         all_markers.markers[i].id = i
     marker_pub.publish(all_markers)
 
-def timer_callback(event):
-    if latest_centroids:
-        publish_markers(frame_id_global, resolution_global, length_x_global, length_y_global, latest_centroids)
-
 def listener():
     rospy.init_node('listener', anonymous=True)
     global marker_pub, raw_marker_pub
     marker_pub = rospy.Publisher("/rock_markers", MarkerArray, queue_size=1)
     raw_marker_pub = rospy.Publisher("/raw_rock_points", MarkerArray, queue_size=1)
-    rospy.Subscriber("/elevation_mapping_large/elevation_map_raw", GridMap, callback)
-    rospy.Timer(rospy.Duration(0.5), timer_callback, reset=True)
+    rospy.Subscriber("/elevation_mapping/elevation_map_raw", GridMap, callback)
     rospy.spin()
 
 if __name__ == '__main__':
